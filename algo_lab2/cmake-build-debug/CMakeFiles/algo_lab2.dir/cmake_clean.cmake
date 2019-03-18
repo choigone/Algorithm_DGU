f96ed363567b753fbb622db9888623e26d6eb725
choigone <gowoonvv@gmail.com>
2019-03-19 01:43:01 +0900

    create lab2

---
+++ algo_lab2/cmake-build-debug/CMakeFiles/algo_lab2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algo_lab2.dir/main.cpp.o"
+  "algo_lab2.pdb"
+  "algo_lab2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algo_lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
