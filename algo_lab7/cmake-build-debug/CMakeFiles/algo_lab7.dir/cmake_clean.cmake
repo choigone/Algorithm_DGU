caafefecc8e9086e1060bd0be20731ff211de018
choigone <gowoonvv@gmail.com>
2019-04-18 16:37:18 +0900

    create lab7

---
+++ algo_lab7/cmake-build-debug/CMakeFiles/algo_lab7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algo_lab7.dir/homework1.cpp.o"
+  "algo_lab7.pdb"
+  "algo_lab7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algo_lab7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ algo_lab7/homework2.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Goun Choi on 2019-04-18.
+//
+
